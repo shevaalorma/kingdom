0fadc5b90e91057e52363c3307f125a61d91aa03
shevaalorma <31123310+shevaalorma@users.noreply.github.com>
2018-06-01 13:27:51 +0800

    Initial commit

--- test11.py
+++ test11.py
@@ -3,6 +3,8 @@ import re
 
 log_line = '''183.60.212.153 - - [19/Feb/2013:10:23:29 +0800] "GET /o2o/media.html?menu=3 HTTP/1.1" 200 16691 "-" "Mozilla/5.0 (compatible; EasouSpider; +http://www.easou.com/search/spider.html)"'''
 
+
+
 def extract(line):
     names = ["remote", "", "", "datetime", "request", "status", "length", "", "useragent"]
     ops = {'datetime':lambda timestr:datetime.datetime.strptime(timestr,"%d/%b/%Y:%H:%M:%S %z"),
@@ -12,9 +14,19 @@ def extract(line):
     pattern_str = '''(?P<remote>[\d.]{7,}) - - \[(?P<datetime>[/\w +:]+)\] "(?P<method>\w+) (?P<url>\S+) (?P<protocol>[\w/.]+)" (?P<status>\d+) (?P<length>\d+) "[^"]+" "(?P<userangent>[^"]+)"'''
     regex = re.compile(pattern_str)
 
-    matcger = regex.match(log_line)
+    matcger = regex.match(line)
     info ={k:ops.get(k,lambda x:x)(v) for k,v in matcger.groupdict().items()}
-    print(info)
+    #print(info)
+    return info
 
+def load(path):
+    with open('test.log') as f:
+        for line in f:
+            fileds = extract(line)
+            if fileds:
+                yield fileds
+            else:
+                continue
+for _ in range(10):
+    print(next(load('.')))
 
-extract(log_line)
