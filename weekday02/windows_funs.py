0c43465d4b12b3b477f9d79a030dd3686a90dda3
shevaalorma <31123310+shevaalorma@users.noreply.github.com>
2018-06-02 11:49:05 +0800

    Initial commit

--- weekday02/windows_funs.py
+++ weekday02/windows_funs.py
@@ -19,3 +19,19 @@ def window(src,handler,width:int,interval:int):
     start =datetime.datetime.strptime('20170101 01:00:00 +0800','%Y%m%d %H:%M:%S %z')
     current =datetime.datetime.strptime('20170101 01:00:00 +0800','%Y%m%d %H:%M:%S %z' )
 
+    buffer = []
+    delta = datetime.timedelta(seconds=width-interval)
+
+    while True:
+        #
+        data = next(src)
+        if data:
+            buffer.append(data)
+            current = data['datetime']
+
+        if (current-start).total_seconds() >= interval:
+            ret = handler(buffer)
+            print("{:.2f}".format(ret))
+            start = current
+
+            buffer = [x for x in buffer if x['datetime'] > current - delta]
