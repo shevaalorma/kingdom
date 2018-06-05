2903272b04f8a961a23a3b0c5454ae21e4927b9b
shevaalorma <31123310+shevaalorma@users.noreply.github.com>
2018-06-05 08:22:03 +0800

    Initial commit

--- weekday02/Offline_log_analysis.py
+++ weekday02/Offline_log_analysis.py
@@ -3,7 +3,34 @@ import datetime
 import time
 from queue import Queue
 import threading
+import re
+import logging
 
+
+log_line = '''183.60.212.153 - - [19/Feb/2013:10:23:29 +0800] "GET /o2o/media.html?menu=3 HTTP/1.1" 200 16691 "-" "Mozilla/5.0 (compatible; EasouSpider; +http://www.easou.com/search/spider.html)"'''
+
+def extract(line) -> dict:
+    names = ["remote", "", "", "datetime", "request", "status", "length", "", "useragent"]
+    ops = {'datetime':lambda timestr:datetime.datetime.strptime(timestr,"%d/%b/%Y:%H:%M:%S %z"),
+           'status': int,
+           'length': int}
+
+    pattern_str = '''(?P<remote>[\d.]{7,}) - - \[(?P<datetime>[/\w +:]+)\] "(?P<method>\w+) (?P<url>\S+) (?P<protocol>[\w/.]+)" (?P<status>\d+) (?P<length>\d+) "[^"]+" "(?P<userangent>[^"]+)"'''
+    regex = re.compile(pattern_str)
+
+    matcger = regex.match(line)
+    info ={k:ops.get(k,lambda x:x)(v) for k,v in matcger.groupdict().items()}
+    #print(info)
+    return info
+
+def load(path):
+    with open(path) as f:
+        for line in f:
+            fileds = extract(line)
+            if fileds:
+                yield fileds
+            else:
+                continue
 
 def source():
     yield {'value':random.randint(1,100),'datetime':datetime.datetime.now()}
@@ -15,7 +42,8 @@ def windows(src:Queue,handler,width:int,interval:int):
     current = datetime.datetime.strptime('2017-01-01 01:00:00', '%Y-%m-%d %H:%M:%S')
     delta = datetime.timedelta(seconds= width - interval)
     buffer = []
-    for data in src.get():
+    while True:
+        data = src.get()
         if data:
             buffer.append(data)
             current = data['datetime']
@@ -25,6 +53,7 @@ def windows(src:Queue,handler,width:int,interval:int):
             print("{:.2f}".format(ret))
             current = start
 
+        #重叠方案
         buffer =[x for x in buffer if x['datetime'] > current -delta ]
 
 
@@ -57,7 +86,9 @@ def donothing_handler(iterable):
 if __name__=="__main__":
     import sys
     path = 'test.log'
-    reg, run = dispatcher(source())
-
+    #字典
+    reg, run = dispatcher(load(path))
+    reg(donothing_handler,10,5)
     run()
-#windows(source(),handler,10,5)
+    logging.info()
+
